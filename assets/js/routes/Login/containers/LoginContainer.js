7fd08ac49f9d5f6f2a9dbe0929c891c110c96723
bluearcon <bestgoldentech@gmail.com>
2017-09-25 00:03:33 +0800

    working with login component

---
+++ assets/js/routes/Login/containers/LoginContainer.js
@@ -0,0 +1,30 @@
+import React, { Component, PropTypes } from 'react'
+import LoginView from '../components/LoginView'
+import Auth from '../../../auth'
+class LoginContainer extends Component {
+	constructor(props) {
+		super(props)
+
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+
+	handleSubmit(username, password) {
+		Auth.login(username, password, (loggedIn) => {
+			if (loggedIn) {
+				this.props.setUser(username)
+				this.props.router.push('/app/')
+			} else {
+				this.setState({ login_error: true })
+			}
+		})
+	}
+
+	render() {
+		return (
+			<LoginView
+				handleSubmit={this.handleSubmit} />
+		)
+	}
+}
+
+export default LoginContainer
