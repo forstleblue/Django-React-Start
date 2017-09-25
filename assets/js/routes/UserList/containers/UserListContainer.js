4341c290aaa2cc776994d6eac8e38c1d812b6317
bluearcon <bestgoldentech@gmail.com>
2017-09-26 04:05:18 +0800

    add npm-log to gitignore

--- assets/js/routes/UserList/containers/UserListContainer.js
+++ assets/js/routes/UserList/containers/UserListContainer.js
@@ -2,12 +2,12 @@ import React, { Component, PropTypes } from 'react'
 import UserListView from '../components/UserListView'
 export default class UserListContainer extends Component {
 	constructor(props) {
-        super(props)
+		super(props)
 	}
 
 	render() {
-        return(
-            <UserListView />
-        )
+		return (
+			<UserListView />
+		)
 	}
 }
