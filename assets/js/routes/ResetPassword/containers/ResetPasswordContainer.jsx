aee4ee60560d641a2585cd37d0c401ad152051bf
bluearcontech <blueskymobile@hotmail.com>
2017-09-30 05:38:07 +0800

    getting user name using sotre

--- assets/js/routes/ResetPassword/containers/ResetPasswordContainer.jsx
+++ assets/js/routes/ResetPassword/containers/ResetPasswordContainer.jsx
@@ -1,69 +1,48 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { changePasswordRequest } from '../../../actions/user'
 import ResetPasswordView from '../components/ResetPasswordView'
-import {changePassword} from '../../../auth'
+import { changePassword } from '../../../auth'
 import { showMessage } from '../../../actions/messages'
 class ResetPasswordContainer extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-			user: {}
-		}
-		this.loadUserData = this.loadUserData.bind(this)
-    this.handlSubmit = this.handlSubmit.bind(this)
-  }
-
-  componentDidMount() {
-		this.loadUserData()
+	constructor(props) {
+		super(props)
+		this.handlSubmit = this.handlSubmit.bind(this)
 	}
 
-  handlSubmit(password) {
-    changePassword(this.state.user.username, password, (res) => {
-			if(res == "password set") {
+	handlSubmit(password) {
+		changePassword(this.props.user.username, password, (res) => {
+			if (res == "password set") {
 				this.refs.passwordView.setState({
 					resetSuccess: true
 				})
 				this.props.dispatch(showMessage("Password changed Successfully."))
-				setTimeout(()=> {
+				setTimeout(() => {
 					this.props.router.push('/app')
 				}, 3000)
 			} else {
 				this.props.dispatch(showMessage("Password Reset Error."))
 			}
 		})
-  }
-
-  loadUserData() {
-		$.ajax({
-			method: 'GET',
-			url: '/api/users/i/',
-			datatype: 'json',
-			headers: {
-				'Authorization': 'Token ' + localStorage.token
-			},
-			success: function (res) {
-				this.setState({ user: res })
-			}.bind(this),
-			fail: function (err) {
-				console.log("error message: " + err.message)
-			}
-		})
 	}
 
-  render() {
-    return (
-      <ResetPasswordView ref="passwordView"
-        onSubmit={this.handlSubmit}
-      />
-    )
-  }
+	render() {
+		return (
+			<ResetPasswordView ref="passwordView"
+				onSubmit={this.handlSubmit}
+			/>
+		)
+	}
 }
 
 ResetPasswordContainer.propTypes = {
-		dispatch: PropTypes.func.isRequired
+	user: PropTypes.object,
+	dispatch: PropTypes.func.isRequired
 }
 
-export default connect()(ResetPasswordContainer)
+const mapStateToProps = state => ({
+  user: state.user,
+})
+
+export default connect(mapStateToProps)(ResetPasswordContainer)
